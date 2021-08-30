122e4ca0b3e787f95136aa3544fdffddafcfdee2
Kapil Gupta <kapilguptansit@gmail.com>
2021-08-30 19:21:50 +0000

    Added Self typed message

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import BaseLayout from '../components/layout/BaseLayout';
 import { Container, Row, Col } from 'reactstrap';
+import Typed from 'react-typed';
+
+const roles = ["Developer", "Tech Lover", "Team Player", "React JS"];
+
 
 class Index extends React.Component {
   render() {
@@ -37,6 +41,17 @@ class Index extends React.Component {
                     Get informed, collaborate and discover projects I was working on through the years!
                   </h1>
                 </div>
+                <Typed 
+                    loop
+                    typeSpeed={70}
+                    backSpeed={70}
+                    strings={roles}
+                    backDelay={1000}
+                    loopCount={0}
+                    showCursor
+                    className="self-typed"
+                    cursorChar="|"
+                />
                 <div className="hero-welcome-bio">
                   <h1>
                     Let's take a look on my work.
