e4672a7f3dd1b6566cc0b457c4bc9ca5f516386d
Kapil Gupta <kapilguptansit@gmail.com>
2021-09-11 15:14:09 +0000

    added mongo connection.

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const server = express();
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+mongoose.connect(`${process.env.DB_URI}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+}, (err) => {
+    if (err) console.error(err);
+    else console.log('Connected to DB!');
+});
 
 const portfolioRoutes = require('./routes/portfolios');
 
